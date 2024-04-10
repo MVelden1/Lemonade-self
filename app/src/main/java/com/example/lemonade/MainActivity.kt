3fe342198ce4c31412c8f56340d24cfc7840af21
Mick <mvelden6@gmail.com>
2024-04-10 21:40:34 +0200

    some additional tweaks

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -12,8 +12,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -79,6 +83,16 @@ fun LemonadeLifeSpan(modifier: Modifier = Modifier) {
         else -> R.string.empty_glass_text
     }
 
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding()
+            .background(MaterialTheme.colorScheme.tertiaryContainer),
+        color = MaterialTheme.colorScheme.background
+    ) {
+
+
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -96,7 +110,8 @@ fun LemonadeLifeSpan(modifier: Modifier = Modifier) {
         ) {
             Text(
                 stringResource(R.string.app_name),
-                fontSize = 20.sp
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
             )
         }
 
@@ -148,7 +163,7 @@ fun LemonadeLifeSpan(modifier: Modifier = Modifier) {
             }
         }
     }
-}
+}}
 
 
 
